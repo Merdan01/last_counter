bf7abe27c40ea8bf679896c4c8b16f12986d4021
Merdan01 <82529843+Merdan01@users.noreply.github.com>
2021-04-17 00:58:11 +0500

    fixed_one

--- script.js
+++ script.js
@@ -2,27 +2,15 @@ console.log(process.argv);
 if(process.argv.length === 2) {
     console.log("please enter argv"); 
     process.exit();
-}
+} 
 
-const fs = require("fs");
+const fs = require("fs"); 
 
-let text = fs.readFileSync(process.argv[2]);
+let text = fs.readFileSync(process.argv[2]); 
 text = text.toString();
 console.log(text);
 
 const res = {};
-
-// for(let c of "")
-// let word="";
-// const set = [];
-// for(let i of text){
-//     if(i!=' ') word+=i;
-//     else{
-//         set.push(word);
-//         word = "";
-//     } 
-// }
-// console.log(set);
 
 const set = text.split(' ');
 
@@ -31,24 +19,23 @@ for(let i of set){
     res[i]++;
 }
 delete res[''];
-// console.log(res);
 
- const arr = [];
-// console.log(Object.keys(res));
+const arr = [];
 
 for(let i in res){
-    let m = [i, res[i]];
-    arr.push(m);
+    arr.push([i, res[i]]);
 }
-// console.log(JSON.stringify(arr,null, 2));
 
-fs.writeFileSync("1.json", JSON.stringify(arr));
+arr.sort(function(a,b){
+    return b[1] - a[1];
+})
+
+const result = [];
+
+for(i of arr){
+    result.push({"word": i[0], "count": i[1]});
+}
+// console.log(result);
 
 
-/*
-[
-    ["word", 1],
-    ["word", 1],
-    ["word", 1],
-]
-*/
+fs.writeFileSync("1.json", JSON.stringify(result));
